bdc4d2ab3b6b0bfba57ec72017fe260b1452189f
efren <1370416578@qq.com>
2025-12-11 16:20:06 +0800

    Update goroutine.go

--- grammar/goroutine.go
+++ grammar/goroutine.go
@@ -5,16 +5,34 @@ import (
 	"time"
 )
 
-func newTask() {
-	i := 0
-	for {
-		i++
-		fmt.Printf("new Goroutine : i = %d\n", i)
-		time.Sleep(1 * time.Second)
-	}
-}
+//func newTask() {
+//	i := 0
+//	for {
+//		i++
+//		fmt.Printf("new Goroutine : i = %d\n", i)
+//		time.Sleep(1 * time.Second)
+//	}
+//}
+//
+//func main() {
+//	go newTask()
+//	fmt.Println("main goroutine exit")
+//}
 
 func main() {
-	go newTask()
-	fmt.Println("main goroutine exit")
+	go func() {
+		defer fmt.Println("A.defer")
+
+		func() {
+			defer fmt.Println("B.defer")
+			// 退出当前goroutine
+			// runtime.Goexit()
+			fmt.Println("B")
+		}()
+		fmt.Println("A")
+	}()
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
 }
