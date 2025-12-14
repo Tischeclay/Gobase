00aac600547b01b46ae4f4557e075a59d0313016
efren <1370416578@qq.com>
2025-12-14 23:46:13 +0800

    Update channel.go

--- grammar/channel.go
+++ grammar/channel.go
@@ -1,16 +1,49 @@
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	c := make(chan int)
+//	go func() {
+//		defer fmt.Println("goroutine exit")
+//		fmt.Println("goroutine running")
+//		c <- 666
+//	}()
+//
+//	num := <-c
+//	fmt.Println("num = ", num)
+//	fmt.Println("main goroutine exit")
+//}
+
+// 无缓冲的channel
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	c := make(chan int)
+	c := make(chan int, 0) //创建无缓冲的通道 c
+
+	//内置函数 len 返回未被读取的缓冲元素数量，cap 返回缓冲区大小
+	fmt.Printf("len(c)=%d, cap(c)=%d\n", len(c), cap(c))
+
 	go func() {
-		defer fmt.Println("goroutine exit")
-		fmt.Println("goroutine running")
-		c <- 666
+		defer fmt.Println("子go程结束")
+
+		for i := 0; i < 3; i++ {
+			c <- i
+			fmt.Printf("子go程正在运行[%d]: len(c)=%d, cap(c)=%d\n", i, len(c), cap(c))
+		}
 	}()
 
-	num := <-c
-	fmt.Println("num = ", num)
-	fmt.Println("main goroutine exit")
+	time.Sleep(2 * time.Second) //延时2s
+
+	for i := 0; i < 3; i++ {
+		num := <-c //从c中接收数据，并赋值给num
+		fmt.Println("num = ", num)
+	}
+
+	fmt.Println("main进程结束")
 }
