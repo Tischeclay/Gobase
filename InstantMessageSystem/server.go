554a9aadc3126de03933e44d52ef97efcaa38444
efren <1370416578@qq.com>
2026-01-15 18:33:26 +0800

    IMSv1.0 success

--- InstantMessageSystem/server.go
+++ InstantMessageSystem/server.go
@@ -1,4 +1,4 @@
-package InstantMessageSystem
+package main
 
 import (
 	"fmt"
@@ -16,35 +16,35 @@ func NewServer(ip string, port int) *Server {
 		Ip:   ip,
 		Port: port,
 	}
+
 	return server
 }
 
-func (s *Server) Handler(conn net.Conn) {
-	// 当前链接的业务
-	fmt.Println("connection succeed")
+func (this *Server) Handler(conn net.Conn) {
+	//...当前链接的业务
+	fmt.Println("链接建立成功")
 }
 
-func (s *Server) Start() {
-	// socket listen
-	// 如果是tcp服务器传递“tcp”，udp服务器传递“udp”
-	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
+// 启动服务器的接口
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
 		fmt.Println("net.Listen err:", err)
 		return
 	}
-
-	// close listen socket
+	//close listen socket
 	defer listener.Close()
 
 	for {
-		// accept
+		//accept
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("listener accept err:", err)
 			continue
 		}
-		// do handler
-		go s.Handler(conn)
-	}
 
+		//do handler
+		go this.Handler(conn)
+	}
 }
