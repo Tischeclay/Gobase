228b3bc1339b42dbb197a8ec8bf4b39bf3752145
efren <1370416578@qq.com>
2026-01-20 23:41:53 +0800

    Update Vector.go

--- golang101/Vector.go
+++ golang101/Vector.go
@@ -1,12 +1,27 @@
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	pm := &map[string]int{"C": 1972, "Go": 2009}
+//	ps := &[]string{"break", "continue"}
+//	pa := &[...]bool{false, true, true, false}
+//	fmt.Printf("%T\n", pm) // *map[string]int
+//	fmt.Printf("%T\n", ps) // *[]string
+//	fmt.Printf("%T\n", pa) // *[4]bool
+//}
+
 package main
 
 import "fmt"
 
 func main() {
-	pm := &map[string]int{"C": 1972, "Go": 2009}
-	ps := &[]string{"break", "continue"}
-	pa := &[...]bool{false, true, true, false}
-	fmt.Printf("%T\n", pm) // *map[string]int
-	fmt.Printf("%T\n", ps) // *[]string
-	fmt.Printf("%T\n", pa) // *[4]bool
-}
+	var a [16]byte
+	var s []int
+	var m map[string]int
+
+	fmt.Println(a == a)   // true
+	fmt.Println(m == nil) // true
+	fmt.Println(s == nil) // true
+	fmt.Println(nil == map[string]int{}) // false
+	fmt.Println(nil == []int{})          // false
