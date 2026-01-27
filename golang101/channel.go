d13f2c5ed4aaa723d19e3a79232e7f93ebd896be
efren <1370416578@qq.com>
2026-01-27 23:42:13 +0800

    Update channel.go

--- golang101/channel.go
+++ golang101/channel.go
@@ -1,25 +1,50 @@
+//package main
+//
+//import (
+//	"fmt"
+//	"time"
+//)
+//
+//func main() {
+//	c := make(chan int) // 一个非缓冲通道
+//	go func(ch chan<- int, x int) {
+//		time.Sleep(time.Second)
+//		// <-ch    // 此操作编译不通过
+//		ch <- x * x // 阻塞在此，直到发送的值被接收
+//	}(c, 3)
+//	done := make(chan struct{})
+//	go func(ch <-chan int) {
+//		n := <-ch      // 阻塞在此，直到有值发送到c
+//		fmt.Println(n) // 9
+//		// ch <- 123   // 此操作编译不通过
+//		time.Sleep(time.Second)
+//		done <- struct{}{}
+//	}(c)
+//	<-done // 阻塞在此，直到有值发送到done
+//	fmt.Println("bye")
+//}
+
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	c := make(chan int) // 一个非缓冲通道
-	go func(ch chan<- int, x int) {
-		time.Sleep(time.Second)
-		// <-ch    // 此操作编译不通过
-		ch <- x * x // 阻塞在此，直到发送的值被接收
-	}(c, 3)
-	done := make(chan struct{})
-	go func(ch <-chan int) {
-		n := <-ch      // 阻塞在此，直到有值发送到c
-		fmt.Println(n) // 9
-		// ch <- 123   // 此操作编译不通过
-		time.Sleep(time.Second)
-		done <- struct{}{}
-	}(c)
-	<-done // 阻塞在此，直到有值发送到done
-	fmt.Println("bye")
+	c := make(chan int, 2) // 一个容量为2的缓冲通道
+	c <- 3
+	c <- 5
+	close(c)
+	fmt.Println(len(c), cap(c)) // 2 2
+	x, ok := <-c
+	fmt.Println(x, ok)          // 3 true
+	fmt.Println(len(c), cap(c)) // 1 2
+	x, ok = <-c
+	fmt.Println(x, ok)          // 5 true
+	fmt.Println(len(c), cap(c)) // 0 2
+	x, ok = <-c
+	fmt.Println(x, ok) // 0 false
+	x, ok = <-c
+	fmt.Println(x, ok)          // 0 false
+	fmt.Println(len(c), cap(c)) // 0 2
+	close(c)                    // 此行将产生一个恐慌
+	c <- 7                      // 如果上一行不存在，此行也将产生一个恐慌。
 }
