4af502be23603f322a9458fd00d21644dd192931
efren <1370416578@qq.com>
2026-01-30 22:34:49 +0800

    Update channel.go

--- golang101/channel.go
+++ golang101/channel.go
@@ -102,16 +102,46 @@
 
 // 一个不含任何分支的select-case代码块select{}将使当前协程处于永久阻塞状态。
 // 在下面这个例子中，default分支将铁定得到执行，因为两个case分支后的操作均为阻塞的。
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	var c chan struct{}
+//	select {
+//	case <-c:
+//	case c <- struct{}{}:
+//	default:
+//		fmt.Println("Go here.")
+//	}
+//}
+
 package main
 
 import "fmt"
 
 func main() {
-	var c chan struct{}
-	select {
-	case <-c:
-	case c <- struct{}{}:
-	default:
-		fmt.Println("Go here.")
+	c := make(chan string, 2)
+	trySend := func(v string) {
+		select {
+		case c <- v:
+		default: // 如果c的缓冲已满，则执行默认分支。
+		}
 	}
+	tryReceive := func() string {
+		select {
+		case v := <-c:
+			return v
+		default:
+			return "-" // 如果c的缓冲为空，则执行默认分支。
+		}
+	}
+	trySend("Hello!") // 发送成功
+	trySend("Hi!")    // 发送成功
+	trySend("Bye!")   // 发送失败，但不会阻塞。
+	// 下面这两行将接收成功。
+	fmt.Println(tryReceive()) // Hello!
+	fmt.Println(tryReceive()) // Hi!
+	// 下面这行将接收失败。
+	fmt.Println(tryReceive()) // -
 }
