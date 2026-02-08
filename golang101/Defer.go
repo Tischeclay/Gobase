640ccb4e0e7c0bc80684aa69b9ae3ed68fa99d02
efren <1370416578@qq.com>
2026-02-08 21:49:20 +0800

    Update Defer.go

--- golang101/Defer.go
+++ golang101/Defer.go
@@ -1,12 +1,24 @@
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	s := []string{"a", "b", "c", "d"}
+//	defer fmt.Println(s) // [a x y d]
+//	// defer append(s[:1], "x", "y") // 编译错误
+//	defer func() {
+//		_ = append(s[:1], "x", "y")
+//	}()
+//}
+
 package main
 
 import "fmt"
 
 func main() {
-	s := []string{"a", "b", "c", "d"}
-	defer fmt.Println(s) // [a x y d]
-	// defer append(s[:1], "x", "y") // 编译错误
-	defer func() {
-		_ = append(s[:1], "x", "y")
-	}()
+	defer fmt.Println("此行可以被执行到")
+	var f func() // f == nil
+	defer f()    // 将产生一个恐慌
+	fmt.Println("此行可以被执行到")
+	f = func() {} // 此行不会阻止恐慌产生
 }
