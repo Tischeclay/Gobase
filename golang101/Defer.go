9bbc62801333165c6e54f15e0dece2191bf61656
efren <1370416578@qq.com>
2026-02-09 23:45:19 +0800

    Update Defer.go

--- golang101/Defer.go
+++ golang101/Defer.go
@@ -11,14 +11,31 @@
 //	}()
 //}
 
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	defer fmt.Println("此行可以被执行到")
+//	var f func() // f == nil
+//	defer f()    // 将产生一个恐慌
+//	fmt.Println("此行可以被执行到")
+//	f = func() {} // 此行不会阻止恐慌产生
+//}
+
 package main
 
-import "fmt"
+type T int
+
+func (t T) M(n int) T {
+	print(n)
+	return t
+}
 
 func main() {
-	defer fmt.Println("此行可以被执行到")
-	var f func() // f == nil
-	defer f()    // 将产生一个恐慌
-	fmt.Println("此行可以被执行到")
-	f = func() {} // 此行不会阻止恐慌产生
+	var t T
+	// t.M(1)是方法调用M(2)的属主实参，因此它
+	// 将在M(2)调用被推入延迟调用栈时被估值。
+	defer t.M(1).M(2)
+	t.M(3).M(4)
 }
