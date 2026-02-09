eff49b49dee225dc6302da4fb4aaf54b04cc05fd
efren <1370416578@qq.com>
2026-02-09 23:46:37 +0800

    Update Defer.go

--- golang101/Defer.go
+++ golang101/Defer.go
@@ -23,19 +23,77 @@
 //	f = func() {} // 此行不会阻止恐慌产生
 //}
 
+//package main
+//
+//type T int
+//
+//func (t T) M(n int) T {
+//	print(n)
+//	return t
+//}
+//
+//func main() {
+//	var t T
+//	// t.M(1)是方法调用M(2)的属主实参，因此它
+//	// 将在M(2)调用被推入延迟调用栈时被估值。
+//	defer t.M(1).M(2)
+//	t.M(3).M(4)
+//}
+
 package main
 
-type T int
+import "os"
 
-func (t T) M(n int) T {
-	print(n)
-	return t
+func withoutDefers(filepath string, head, body []byte) error {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Seek(16, 0)
+	if err != nil {
+		f.Close()
+		return err
+	}
+
+	_, err = f.Write(head)
+	if err != nil {
+		f.Close()
+		return err
+	}
+
+	_, err = f.Write(body)
+	if err != nil {
+		f.Close()
+		return err
+	}
+
+	err = f.Sync()
+	f.Close()
+	return err
 }
 
-func main() {
-	var t T
-	// t.M(1)是方法调用M(2)的属主实参，因此它
-	// 将在M(2)调用被推入延迟调用栈时被估值。
-	defer t.M(1).M(2)
-	t.M(3).M(4)
+func withDefers(filepath string, head, body []byte) error {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Seek(16, 0)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(head)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(body)
+	if err != nil {
+		return err
+	}
+
+	return f.Sync()
 }
