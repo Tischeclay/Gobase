e8725d7eb26b5408042c4ab20e1a9c725ee0e895
efren <1370416578@qq.com>
2026-02-11 20:17:19 +0800

    Update Defer.go

--- golang101/Defer.go
+++ golang101/Defer.go
@@ -113,19 +113,20 @@ m.Unlock()
 }
 func writeManyFiles(files []File) error {
 for _, file := range files {
+if err := func() error {
 f, err := os.Open(file.path)
 if err != nil {
 return err
 }
-defer f.Close()
+defer f.Close() // 将在此循环步内执行
 
 _, err = f.WriteString(file.content)
 if err != nil {
 return err
 }
 
-err = f.Sync()
-if err != nil {
+return f.Sync()
+}(); err != nil {
 return err
 }
 }
