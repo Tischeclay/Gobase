b3ae760ed022ff432940357afc401710bed6937a
efren <1370416578@qq.com>
2026-02-17 23:09:16 +0800

    Update Fuzhi.go

--- golang101/Fuzhi.go
+++ golang101/Fuzhi.go
@@ -1,17 +1,41 @@
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	f := func(n int) int {
+//		fmt.Printf("f(%v) is called.\n", n)
+//		return n
+//	}
+//
+//	switch x := f(3); x + f(4) {
+//	default:
+//	case f(5):
+//	case f(6), f(7), f(8):
+//	case f(9), f(10):
+//	}
+//}
+
 package main
 
 import "fmt"
 
 func main() {
-	f := func(n int) int {
-		fmt.Printf("f(%v) is called.\n", n)
-		return n
+	c := make(chan int, 1)
+	c <- 0
+	fchan := func(info string, c chan int) chan int {
+		fmt.Println(info)
+		return c
+	}
+	fptr := func(info string) *int {
+		fmt.Println(info)
+		return new(int)
 	}
 
-	switch x := f(3); x + f(4) {
-	default:
-	case f(5):
-	case f(6), f(7), f(8):
-	case f(9), f(10):
+	select {
+	case *fptr("aaa") = <-fchan("bbb", nil): // blocking
+	case *fptr("ccc") = <-fchan("ddd", c): // non-blocking
+	case fchan("eee", nil) <- *fptr("fff"): // blocking
+	case fchan("ggg", nil) <- *fptr("hhh"): // blocking
 	}
 }
