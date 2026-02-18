17b8bf8a705c1f3ce67e7a307e1319b02692fa71
efren <1370416578@qq.com>
2026-02-18 23:26:57 +0800

    Update Fuzhi.go

--- golang101/Fuzhi.go
+++ golang101/Fuzhi.go
@@ -16,26 +16,64 @@
 //	}
 //}
 
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	c := make(chan int, 1)
+//	c <- 0
+//	fchan := func(info string, c chan int) chan int {
+//		fmt.Println(info)
+//		return c
+//	}
+//	fptr := func(info string) *int {
+//		fmt.Println(info)
+//		return new(int)
+//	}
+//
+//	select {
+//	case *fptr("aaa") = <-fchan("bbb", nil): // blocking
+//	case *fptr("ccc") = <-fchan("ddd", c): // non-blocking
+//	case fchan("eee", nil) <- *fptr("fff"): // blocking
+//	case fchan("ggg", nil) <- *fptr("hhh"): // blocking
+//	}
+//}
+
 package main
 
-import "fmt"
+import "testing"
 
-func main() {
-	c := make(chan int, 1)
-	c <- 0
-	fchan := func(info string, c chan int) chan int {
-		fmt.Println(info)
-		return c
-	}
-	fptr := func(info string) *int {
-		fmt.Println(info)
-		return new(int)
-	}
+type S [12]int64
 
-	select {
-	case *fptr("aaa") = <-fchan("bbb", nil): // blocking
-	case *fptr("ccc") = <-fchan("ddd", c): // non-blocking
-	case fchan("eee", nil) <- *fptr("fff"): // blocking
-	case fchan("ggg", nil) <- *fptr("hhh"): // blocking
+var sX = make([]S, 1000)
+var sY = make([]S, 1000)
+var sZ = make([]S, 1000)
+var sumX, sumY, sumZ int64
+
+func Benchmark_Loop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sumX = 0
+		for j := 0; j < len(sX); j++ {
+			sumX += sX[j][0]
+		}
+	}
+}
+
+func Benchmark_Range_OneIterVar(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sumY = 0
+		for j := range sY {
+			sumY += sY[j][0]
+		}
+	}
+}
+
+func Benchmark_Range_TwoIterVar(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		sumZ = 0
+		for _, v := range sZ {
+			sumZ += v[0]
+		}
 	}
 }
