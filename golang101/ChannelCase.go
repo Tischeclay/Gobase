9fdc106ba8c99209ee9872e7f41714becc0b8e0e
efren <1370416578@qq.com>
2026-02-26 23:56:55 +0800

    Update ChannelCase.go

--- golang101/ChannelCase.go
+++ golang101/ChannelCase.go
@@ -137,6 +137,58 @@
 //	fmt.Println(IsClosed(c)) //true
 //}
 
+//package main
+//
+//import (
+//	"log"
+//	"math/rand"
+//	"sync"
+//	"time"
+//)
+//
+//func main() {
+//	rand.Seed(time.Now().UnixNano()) // Go 1.20之前需要
+//	log.SetFlags(0)
+//
+//	// ...
+//	const Max = 100000
+//	const NumReceivers = 100
+//
+//	wgReceivers := sync.WaitGroup{}
+//	wgReceivers.Add(NumReceivers)
+//
+//	// ...
+//	dataCh := make(chan int)
+//
+//	// 发送者
+//	go func() {
+//		for {
+//			if value := rand.Intn(Max); value == 0 {
+//				// 此唯一的发送者可以安全地关闭此数据通道。
+//				close(dataCh)
+//				return
+//			} else {
+//				dataCh <- value
+//			}
+//		}
+//	}()
+//
+//	// 接收者
+//	for i := 0; i < NumReceivers; i++ {
+//		go func() {
+//			defer wgReceivers.Done()
+//
+//			// 接收数据直到通道dataCh已关闭
+//			// 并且dataCh的缓冲队列已空。
+//			for value := range dataCh {
+//				log.Println(value)
+//			}
+//		}()
+//	}
+//
+//	wgReceivers.Wait()
+//}
+
 package main
 
 import (
@@ -152,39 +204,61 @@ func main() {
 
 	// ...
 	const Max = 100000
-	const NumReceivers = 100
+	const NumSenders = 1000
 
 	wgReceivers := sync.WaitGroup{}
-	wgReceivers.Add(NumReceivers)
+	wgReceivers.Add(1)
 
 	// ...
 	dataCh := make(chan int)
+	stopCh := make(chan struct{})
+	// stopCh是一个额外的信号通道。它的
+	// 发送者为dataCh数据通道的接收者。
+	// 它的接收者为dataCh数据通道的发送者。
 
 	// 发送者
-	go func() {
-		for {
-			if value := rand.Intn(Max); value == 0 {
-				// 此唯一的发送者可以安全地关闭此数据通道。
-				close(dataCh)
-				return
-			} else {
-				dataCh <- value
-			}
-		}
-	}()
-
-	// 接收者
-	for i := 0; i < NumReceivers; i++ {
+	for i := 0; i < NumSenders; i++ {
 		go func() {
-			defer wgReceivers.Done()
+			for {
+				// 这里的第一个尝试接收用来让此发送者
+				// 协程尽早地退出。对于这个特定的例子，
+				// 此select代码块并非必需。
+				select {
+				case <-stopCh:
+					return
+				default:
+				}
 
-			// 接收数据直到通道dataCh已关闭
-			// 并且dataCh的缓冲队列已空。
-			for value := range dataCh {
-				log.Println(value)
+				// 即使stopCh已经关闭，此第二个select
+				// 代码块中的第一个分支仍很有可能在若干个
+				// 循环步内依然不会被选中。如果这是不可接受
+				// 的，则上面的第一个select代码块是必需的。
+				select {
+				case <-stopCh:
+					return
+				case dataCh <- rand.Intn(Max):
+				}
 			}
 		}()
 	}
 
+	// 接收者
+	go func() {
+		defer wgReceivers.Done()
+
+		for value := range dataCh {
+			if value == Max-1 {
+				// 此唯一的接收者同时也是stopCh通道的
+				// 唯一发送者。尽管它不能安全地关闭dataCh数
+				// 据通道，但它可以安全地关闭stopCh通道。
+				close(stopCh)
+				return
+			}
+
+			log.Println(value)
+		}
+	}()
+
+	// ...
 	wgReceivers.Wait()
 }
