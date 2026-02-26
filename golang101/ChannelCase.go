e8db2de952c4c48003ea6618071ee1fc68082988
efren <1370416578@qq.com>
2026-02-26 23:47:13 +0800

    Update ChannelCase.go

--- golang101/ChannelCase.go
+++ golang101/ChannelCase.go
@@ -115,24 +115,76 @@
 //	fmt.Println(values[0], values[len(values)-1])
 //}
 
+//package main
+//
+//import "fmt"
+//
+//type T int
+//
+//func IsClosed(ch <-chan T) bool {
+//	select {
+//	case <-ch:
+//		return true
+//	default:
+//	}
+//	return false
+//}
+//
+//func main() {
+//	c := make(chan T)
+//	fmt.Println(IsClosed(c)) // false
+//	close(c)
+//	fmt.Println(IsClosed(c)) //true
+//}
+
 package main
 
-import "fmt"
-
-type T int
-
-func IsClosed(ch <-chan T) bool {
-	select {
-	case <-ch:
-		return true
-	default:
-	}
-	return false
-}
+import (
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
 
 func main() {
-	c := make(chan T)
-	fmt.Println(IsClosed(c)) // false
-	close(c)
-	fmt.Println(IsClosed(c)) //true
+	rand.Seed(time.Now().UnixNano()) // Go 1.20之前需要
+	log.SetFlags(0)
+
+	// ...
+	const Max = 100000
+	const NumReceivers = 100
+
+	wgReceivers := sync.WaitGroup{}
+	wgReceivers.Add(NumReceivers)
+
+	// ...
+	dataCh := make(chan int)
+
+	// 发送者
+	go func() {
+		for {
+			if value := rand.Intn(Max); value == 0 {
+				// 此唯一的发送者可以安全地关闭此数据通道。
+				close(dataCh)
+				return
+			} else {
+				dataCh <- value
+			}
+		}
+	}()
+
+	// 接收者
+	for i := 0; i < NumReceivers; i++ {
+		go func() {
+			defer wgReceivers.Done()
+
+			// 接收数据直到通道dataCh已关闭
+			// 并且dataCh的缓冲队列已空。
+			for value := range dataCh {
+				log.Println(value)
+			}
+		}()
+	}
+
+	wgReceivers.Wait()
 }
