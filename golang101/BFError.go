50fb605642b81c5460157e7ad632a50d4ca18b36
efren <1370416578@qq.com>
2026-03-03 23:53:05 +0800

    Create BFError.go

---
+++ golang101/BFError.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"runtime"
+	"time"
+)
+
+func main() {
+	var a []int // nil
+	var b bool  // false
+
+	// 一个匿名协程。
+	go func() {
+		a = make([]int, 3)
+		b = true // 写入b
+	}()
+
+	for !b { // 读取b
+		time.Sleep(time.Second)
+		runtime.Gosched()
+	}
+	a[0], a[1], a[2] = 0, 1, 2 // 可能会发生恐慌
+}
