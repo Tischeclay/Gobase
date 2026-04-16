6d9a4528acd0309065434c25770a54bd23c356c6
efren <1370416578@qq.com>
2026-04-16 23:23:37 +0800

    Update heapsort.go

--- leetcode/heapsort.go
+++ leetcode/heapsort.go
@@ -91,53 +91,6 @@ func heapifyIterative(arr []int, n, i int) {
 	}
 }
 
-func mergeSortedChunks(arr []int, chunks [][]int) {
-	type Item struct {
-		value int
-		chunk int
-		index int
-	}
-
-	// 创建最小堆
-	heap := make([]Item, 0, len(chunks))
-	for i, chunk := range chunks {
-		if len(chunk) > 0 {
-			heap = append(heap, Item{chunk[0], i, 0})
-		}
-	}
-
-	// 构建堆
-	for i := len(heap)/2 - 1; i >= 0; i-- {
-		heapifyItems(heap, i)
-	}
-
-	// 合并
-	idx := 0
-	for len(heap) > 0 {
-		// 取出最小值
-		item := heap[0]
-		arr[idx] = item.value
-		idx++
-
-		// 从对应的块中取下一个元素
-		if item.index+1 < len(chunks[item.chunk]) {
-			heap[0] = Item{
-				value: chunks[item.chunk][item.index+1],
-				chunk: item.chunk,
-				index: item.index + 1,
-			}
-			heapifyItems(heap, 0)
-		} else {
-			// 移除该块
-			heap[0] = heap[len(heap)-1]
-			heap = heap[:len(heap)-1]
-			if len(heap) > 0 {
-				heapifyItems(heap, 0)
-			}
-		}
-	}
-}
-
 func heapifyItems(heap []Item, i int) {
 	n := len(heap)
 	smallest := i
