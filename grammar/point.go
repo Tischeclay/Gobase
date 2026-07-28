0958d47de4a93d9e877b9a1dd04fa2a498711e19
efren <1370416578@qq.com>
2026-07-28 22:58:33 +0800

    Update point.go

--- grammar/point.go
+++ grammar/point.go
@@ -1,14 +1,37 @@
+//package main
+//
+//import "fmt"
+//
+//// *是对指针取值，&是对变量取址
+//func changeValue(p *int) {
+//	*p = 10
+//}
+//
+//func main() {
+//	var a int = 1
+//	changeValue(&a)
+//	fmt.Println("a=", a)
+//}
+
 package main
 
 import "fmt"
 
-// *是对指针取值，&是对变量取址
-func changeValue(p *int) {
-	*p = 10
-}
-
 func main() {
-	var a int = 1
-	changeValue(&a)
-	fmt.Println("a=", a)
+	var a, b int = 20, 30 // 声明实际变量
+	var ptra *int         // 声明指针变量
+	var ptrb *int = &b
+
+	ptra = &a // 指针变量的存储地址
+
+	fmt.Printf("a  变量的地址是: %x\n", &a)
+	fmt.Printf("b  变量的地址是: %x\n", &b)
+
+	// 指针变量的存储地址
+	fmt.Printf("ptra  变量的存储地址: %x\n", ptra)
+	fmt.Printf("ptrb  变量的存储地址: %x\n", ptrb)
+
+	// 使用指针访问值
+	fmt.Printf("*ptra  变量的值: %d\n", *ptra)
+	fmt.Printf("*ptrb  变量的值: %d\n", *ptrb)
 }
